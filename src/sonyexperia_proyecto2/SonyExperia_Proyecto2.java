dadeb916457ae195fa7f2405fe9243c67e436702
jesus13santi <jesus13santi@gmail.com>
2022-06-15 15:10:46 -0400

    Primer commit

---
+++ src/sonyexperia_proyecto2/SonyExperia_Proyecto2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonyexperia_proyecto2;
+
+/**
+ *
+ * @author jesus13santi
+ */
+public class SonyExperia_Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
